9853d5ba325b3ee9c193b512e81e4e84dae9ff78
mayanksharma302 <mkps3112@gmail.com>
2026-07-29 18:17:41 +0530

    clean up import statement for config in socket service

--- server/src/services/socket.service.js
+++ server/src/services/socket.service.js
@@ -1,7 +1,7 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import express from 'express';
-import config from '../config/config.js'; // Import your config if needed for CORS
+import config from '../config/config.js';
 
 const app = express();
 const server = http.createServer(app);
